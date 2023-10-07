8405079a3c83fa79a8a47c624ba12f76b5ff0073
tsukisyd <3100277385@qq.com>
2023-10-07 10:21:59 +0800

    自动推送

--- 01-hello.py
+++ 01-hello.py
@@ -1,1 +1,1 @@
-print()
+print('这是一个python程序')
---
+++ push.sh
@@ -0,0 +1,6 @@
+git config --global user.name'tsukisyd'
+git config --global user.email'3100277385@qq.com'
+git config --global http.sslverify false
+git add .
+git commit -m '自动推送'
+git push
