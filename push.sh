1cb22f4c1e903b9c72da549adea342c8c491de3f
tsukisyd <3100277385@qq.com>
2023-10-07 10:26:56 +0800

    自动推送

--- push.sh
+++ push.sh
@@ -1,5 +1,5 @@
-git config --global user.name'tsukisyd'
-git config --global user.email'3100277385@qq.com'
+git config --global user.name 'tsukisyd'
+git config --global user.email '3100277385@qq.com'
 git config --global http.sslverify false
 git add .
 git commit -m '自动推送'
